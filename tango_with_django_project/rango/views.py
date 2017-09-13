68ba752952493ca54cb0eb0953e7f83cdc607095
bonyage <youyouzhixin@gmail.com>
2017-09-14 02:17:16 +1000

    Templetes, static files and media files
    Configure the settings.py file to enable the application use templates, static files and media files

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    # Construct a dictionary to pass to the template engine as its context.
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to the client.
+    return render(request, 'rango/index.html', context=context_dict)
 
